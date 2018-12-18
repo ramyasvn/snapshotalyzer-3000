294cc839b0a01b97370e015804fb9c53a08cdedb
Venkatesh Patil <venkateshpatil@Venkateshs-Air.fios-router.home>
2018-12-18 14:16:28 -0500

    Stopping and starting instances before snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -72,7 +73,7 @@ def list_volumes(project):
 def instances():
     """ Commands For instances """
 
-@instance.command('snapshots',
+@instances.command('snapshots',
 help = "Create snapshots for all volumes")
 @click.option('--project', default=None,
 help="Only instances for project(tag Project:<name>)")
@@ -83,12 +84,23 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by snapshotalyzer 3000")
 
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+        print("Job is done!")
+
     return
-    
+
 @instances.command('list')
 @click.option('--project', default=None,
  help="Only instances for project(tag Project:<name>)")
@@ -121,7 +133,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...", format(i.id))
+        try:
         i.start()
+    except botocore.exceptions.ClientError as e:
+        print("Could not start {0}...".format(i.id)+str(e))
+        continue
     return
 
 @instances.command('stop')
@@ -135,7 +151,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...", format(i.id))
+        try:
         i.stop()
+    except botocore.exceptions.ClientError as e:
+        print("Could not stop {0}...".format(i.id)+str(e))
+        continue
     return
 
 if __name__ == '__main__':
